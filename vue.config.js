a812809a0f9dbb3aa14d3e77023e9157858c3fd7
kushmat <kthapa@deerhold.org>
2022-05-15 20:55:44 +0545

    KT: Add project name in path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/project name/',
+  publicPath: '/dautari/',
   transpileDependencies: true,
   lintOnSave: false,
 })
